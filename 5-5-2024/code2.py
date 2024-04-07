a4639f45f9a023843cd948c44f47ee1f26fda6c5
Krishna-io <143192474+kirshna-io@users.noreply.github.com>
2024-04-07 21:51:32 +0530

    Create code2.py

---
+++ 5-5-2024/code2.py
@@ -0,0 +1,8 @@
+//check positive or negative
+num = int(input())
+if(num>0):
+    print("the num are postive")
+elif(num == 0):
+    print("the num are zero")
+else:
+    print("the num are negative")
